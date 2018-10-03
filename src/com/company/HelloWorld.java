e901aa5f4f92091ade04308e96524f7d6f35fc8c
martyheyn <heynmarty@gmail.com>
2018-10-03 16:30:04 -0400

    Initial commit HelloWorld Oct3

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.company;
+
+//public class HelloWorld {
+
+    public class HelloWorld {
+        public static void main(String[] args) {
+            System.out.println("Hello, World!");
+            System.out.println("Today is July 19, 2015. I am alive!");
+            System.out.println("My name is Gilda.");
+        }
+    }
+
